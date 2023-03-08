ff0f80ca247b4e2a2033f145b4bc3b6a9fdf8dcb
Jari Pennanen <ciantic@oksidi.com>
2023-03-08 18:38:15 +0200

    ..?

--- ui/TotalsCache.ts
+++ ui/TotalsCache.ts
@@ -1,5 +1,6 @@
 import { startOfDay, subDays } from "https://cdn.skypack.dev/date-fns";
 import { C } from "../../../../Users/jarip/AppData/Local/deno/npm/registry.npmjs.org/@tauri-apps/api/1.2.0/event-2a9960e7.d.ts";
+import { ProjectMonitoringDb } from "./ProjectMonitoringDb.ts";
 import { asDefaultMap, DefaultMap } from "./utils/asDefaultMap.ts";
 import { simpleMapEvent } from "./utils/simpleMapEvent.ts";
 import { getDailyTotals, splitTotals, splitTotalsFrom } from "./utils/splitTotals.ts";
@@ -40,53 +41,22 @@ function key(client: string, project: string): ClientAndProjectKey {
     return `${client}~${project}`;
 }
 
-export class ProjectMonitoringDb {
-    private client = "";
-    private project = "";
-    private start?: Date;
-
-    private timings: PersistedTiming[] = [];
+export class TotalsCache {
     private dailyTotalsAsProjectAndClient = asDefaultMap<
         ClientAndProjectKey,
         DefaultMap<DayTimestamp, TotalHours>
     >(() => asDefaultMap<DayTimestamp, TotalHours>(() => 0));
     private apiLoadedClientsAndProjects = new Set<ClientAndProjectKey>();
 
-    // Event listener hookup
-    public onInsertTiming = simpleMapEvent<PersistedTiming, void>(this);
-
-    constructor() {
-        // TODO: Periodically save timings to database
+    constructor(private db: ProjectMonitoringDb) {
+        db.onInsertTiming.addListener(this.onDbInsertTiming, this);
     }
 
-    public destroy() {}
-
-    public startTiming({ client, project }: { client: string; project: string }, now = new Date()) {
-        console.log("Start timing", client, project);
-        if (this.start) {
-            throw new Error("Already timing");
-        }
-        this.client = client;
-        this.project = project;
-        this.start = new Date(now);
+    public destroy() {
+        this.db.onInsertTiming.removeListener(this.onDbInsertTiming);
     }
 
-    public stopTiming(now = new Date()) {
-        console.log("Stop timing");
-        if (this.start) {
-            this.insertTiming({
-                client: this.client,
-                project: this.project,
-                start: this.start,
-                end: now,
-            });
-            this.start = undefined;
-        } else {
-            throw new Error("Not timing");
-        }
-    }
-
-    private insertTiming(timing: PersistedTiming) {
+    private onDbInsertTiming(timing: PersistedTiming) {
         console.log("Insert timing", timing);
 
         // Add to daily totals
@@ -96,28 +66,23 @@ export class ProjectMonitoringDb {
                 .getDefault(clientProject)
                 .updateDefault(day, (value) => value + total);
         }
-
-        // Insert timing
-        this.timings.push(timing);
-
-        // Send event to listeners
-        this.onInsertTiming.trigger(timing);
     }
 
     public getTotals(
-        { client, project }: { client: string; project: string },
+        currentTiming: PersistedTiming,
         now = new Date()
     ): {
         totals: Totals;
 
         // `updateFromDb` is returned only if this client and project pair
         // doesn't have database totals in cache
-        updateFromDb?: () => Promise<Totals>;
+        updateFromDb?: () => Promise<void>;
     } {
+        const { client, project, start, end } = currentTiming;
         const kv = key(client, project);
         const dailyTotals = this.dailyTotalsAsProjectAndClient.getDefault(kv);
         const dailyTotalsSum = splitTotalsFrom(dailyTotals, now);
-        const currentTotals = this.getRunningTotal({ client, project }, now);
+        const currentTotals = splitTotals([currentTiming]);
         const apiTotals = this.updateDailyTotalsFromApi({ client, project });
         if (typeof apiTotals === "function") {
             return {
@@ -128,29 +93,13 @@ export class ProjectMonitoringDb {
                             console.error("API Error", error);
                         })
                         .then(() => {
-                            return this.getTotals({ client, project }, new Date()).totals;
+                            // return this.getTotals({ client, project }, new Date()).totals;
                         });
                 },
             };
         }
         return {
             totals: sumNTotals(dailyTotalsSum, currentTotals),
-        };
-    }
-
-    private getRunningTotal(
-        { client, project }: { client: string; project: string },
-        now = new Date()
-    ): Totals {
-        if (this.client === client && this.project === project && this.start) {
-            return splitTotals([{ start: this.start, end: now }]);
-        }
-        return {
-            todayTotal: 0,
-            thisWeekTotal: 0,
-            lastWeekTotal: 0,
-            eightWeekTotal: 0,
-            total: 0,
         };
     }
 
---
+++ ui/utils/simpleMapEvent.ts
@@ -0,0 +1,112 @@
+export function simpleMapEvent<TParam, TRes = void, TContext = unknown>(context?: TContext) {
+    type T = { (this: TContext, inputs: TParam): TRes };
+    return {
+        cbs: [] as [T, T][], // First value is used for equality check, second is the bound version
+        addListener(cb: T, bindThis?: unknown) {
+            return this.cbs.push([cb, bindThis ? cb.bind(bindThis as TContext) : cb]);
+        },
+        removeListener(cb: T) {
+            const i = this.cbs.findIndex(([v, _]) => v == cb);
+            if (i >= 0) return this.cbs.splice(i, 1);
+            return [];
+        },
+        trigger(trigger: TParam) {
+            return this.cbs.map(([_, cb]) => cb.call(context as TContext, trigger) as TRes);
+        },
+        triggerHandled(trigger: TParam) {
+            return this.cbs.some(([_, cb]) => !!cb.call(context as TContext, trigger));
+        },
+    };
+}
+
+if (typeof Deno !== "undefined" && Deno) {
+    Deno.test("Listener gets the value", () => {
+        const event = simpleMapEvent<{ foo: number }, void>();
+        let value = 0;
+        event.addListener((v) => {
+            value = v.foo;
+        });
+        event.trigger({ foo: 1 });
+        if (value !== 1) {
+            throw new Error("Expected value to be 1");
+        }
+    });
+
+    Deno.test("Trigger returns all values", () => {
+        const event = simpleMapEvent<void, string>();
+
+        event.addListener(() => {
+            return "Hello World";
+        });
+        event.addListener(() => {
+            return "I'm another one";
+        });
+
+        const ret = event.trigger();
+        if (ret[0] !== "Hello World" || ret[1] !== "I'm another one") {
+            throw new Error("Expected values to match");
+        }
+    });
+
+    Deno.test("Trigger handled halts", () => {
+        const event = simpleMapEvent<void, string>();
+        const executed = [];
+
+        event.addListener(() => {
+            executed.push(1);
+            return "Hello World";
+        });
+        event.addListener(() => {
+            executed.push(2);
+            // This does not get executed, because first one returned trueish value
+            return "I'm another one";
+        });
+
+        const ret = event.triggerHandled();
+        if (ret !== true || executed.length !== 1) {
+            throw new Error("Expected values to match");
+        }
+    });
+    Deno.test("Allow using prototype", () => {
+        class Example {
+            test = 1;
+            onSomeEvent() {
+                this.test += 1;
+            }
+        }
+
+        const state = new Example();
+        const event = simpleMapEvent<void, void>();
+
+        // Notice that addListener takes in the context, this means you can use
+        // the prototype functions directly
+        event.addListener(state.onSomeEvent, state);
+        event.trigger();
+        event.removeListener(state.onSomeEvent);
+
+        if (state.test !== 2) {
+            throw new Error("Expected test to be 2");
+        }
+        if (event.cbs.length !== 0) {
+            throw new Error("Expected listeners to be removed");
+        }
+    });
+
+    Deno.test("Test using custom context", () => {
+        const state = { value: 1 };
+        const event = simpleMapEvent<void, void, typeof state>(state);
+
+        // Now that state is given, the listener can access the value
+        event.addListener(function () {
+            // typeof this == typeof state;
+            this.value += 1;
+        });
+
+        event.trigger();
+
+        if (state.value !== 2) {
+            throw new Error("Expected test to be 2");
+        }
+        console.log("cbs", event.cbs);
+    });
+}
